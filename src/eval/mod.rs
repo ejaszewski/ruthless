6c318ff1db984e02aa8d2aa067fb587ed0198b40
Ethan Jaszewski <ethanjaszewski@yahoo.com>
2017-12-11 13:26:04 -0800

    Improved evaluation function.

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -4,19 +4,45 @@ pub mod search;
 
 use ::board;
 
+// A B C D D C B A
+// B B E F F E B B
+// C E G F F G E C
+// D F F E E F F D
+const SCORE_FUNC: [(u64, f32); 7] = [
+    (0x81_00_00_00_00_00_00_81, 30.0), // A
+    (0x42_C3_00_00_00_00_C3_42,  5.0), // B
+    (0x24_00_81_00_00_81_00_24, 15.0), // C
+    (0x18_00_00_81_81_00_00_18, 10.0), // D
+    (0x00_24_42_00_00_42_24_00,  1.0), // E
+    (0x00_18_18_66_66_18_18_00,  2.0), // F
+    (0x00_00_24_00_00_24_00_00,  5.0)  // G
+];
+
+const MATERIAL_WEIGHT: f32 = 0.1;
+const MOBILITY_WEIGHT: f32 = 0.5;
+
+fn disk_count(x: u64, mask: u64) -> f32 {
+    return (x & mask).count_ones() as f32
+}
+
 pub fn get_score(board: &board::Board) -> f32 {
+    let mut eval: f32 = 0.0;
+    for sc in SCORE_FUNC.iter() {
+        eval += (disk_count(board.dark_disks, sc.0) - disk_count(board.light_disks, sc.0)) * sc.1;
+    }
+    eval *= MATERIAL_WEIGHT;
+    eval += board.get_moves().len() as f32 * MOBILITY_WEIGHT;
     if board.dark_move {
-        let eval = board.dark_disks.count_ones() as f32 - board.light_disks.count_ones() as f32;
-        eprintln!("Dark Evaluation: {}", eval);
         eval
     } else {
-        let eval = board.light_disks.count_ones() as f32 - board.dark_disks.count_ones() as f32;
-        eprintln!("Light Evaluation: {}", eval);
-        eval
+        -eval
     }
 }
 
 pub fn do_search(board: &mut board::Board) -> Option<u8> {
+
+    eprintln!("Current board score: {}", get_score(board));
+
     let moves = board.get_moves();
     if moves.len() == 0 {
         return None
@@ -30,7 +56,7 @@ pub fn do_search(board: &mut board::Board) -> Option<u8> {
 
     for m in &moves {
         let undo = board.make_move(Some(*m));
-        let (m_score, leaves) = search::negamax(board, -10000., 10000., 2);
+        let (m_score, leaves) = search::negamax(board, -10000., 10000., 7);
         let m_score = -m_score;
         searched += leaves;
         board.undo_move(undo, *m);
@@ -41,9 +67,10 @@ pub fn do_search(board: &mut board::Board) -> Option<u8> {
     }
 
     let end_time = time::now();
-    let time_taken: time::Duration = end_time - start_time;
+    let time_taken: time::Duration = (end_time - start_time).num_milliseconds();
+    let nps = searched as f32 / time_taken as f32
 
+    eprintln!("Searched {} nodes in {} millis. ({} nodes/sec)", searched, time_taken, nps);
     eprintln!("Making move {} with score {}.", best_move, score);
-    eprintln!("Searched {} nodes in {} millis.", searched, time_taken.num_milliseconds());
     return Some(best_move)
 }
