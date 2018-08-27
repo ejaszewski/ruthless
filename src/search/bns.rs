9fecf73e1545de5d8dcfa558d2eb3e307d523012
Ethan Jaszewski <ethanjaszewski@yahoo.com>
2018-08-26 21:37:55 -0600

    Added some more printing to the BNS root call.

--- src/search/bns.rs
+++ src/search/bns.rs
@@ -12,6 +12,14 @@ pub fn best_node_search<T: Evaluator>(board: &mut Board, depth: u8, evaluator: &
         a + ((b - a) as f32 * ((count as f32 - 1.0) / count as f32)) as i32
     };
 
+    let time_ms = | start: Instant, end: Instant | {
+        let duration = end - start;
+        let secs = duration.as_secs();
+        let millis = duration.subsec_millis();
+
+        (secs as u32 * 1000) + millis
+    };
+
     let (initial, _) = negamax_impl(board, -i32::MAX, i32::MAX, depth / 2, evaluator);
     let mut alpha = initial - 20;
     let mut beta = initial + 20;
@@ -19,9 +27,15 @@ pub fn best_node_search<T: Evaluator>(board: &mut Board, depth: u8, evaluator: &
 
     println!("Running BNS:");
 
+    let total_time_start = Instant::now();
+    let mut total_nodes = 0;
+
     let mut moves = board.get_moves();
 
     while beta - alpha >= 2 && better != 1 {
+        let iter_time_start = Instant::now();
+        let mut iter_nodes = 0;
+
         let guess = next_guess(alpha, beta, better);
 
         print!("  - α: {}, β: {}, G: {}", alpha, beta, guess);
@@ -33,9 +47,12 @@ pub fn best_node_search<T: Evaluator>(board: &mut Board, depth: u8, evaluator: &
             board.undo_move(undo, *m);
 
             result = -result;
+            iter_nodes += nodes;
 
             result >= guess
         }).map(|m| *m).collect();
+
+        total_nodes += iter_nodes;
 
         if filtered.len() >= 1 {
             alpha = guess;
@@ -45,8 +62,10 @@ pub fn best_node_search<T: Evaluator>(board: &mut Board, depth: u8, evaluator: &
             beta = guess;
         }
 
-        println!(" -- Better: {}", better);
+        println!(" -- Time: {} ms, Nodes: {} -- Better: {}", time_ms(iter_time_start, Instant::now()), iter_nodes, better);
     }
+
+    println!("BNS Finished. Time: {} ms, Nodes: {}, Best Move: {}", time_ms(total_time_start, Instant::now()), total_nodes, moves[0]);
 
     (alpha, moves[0])
 }
